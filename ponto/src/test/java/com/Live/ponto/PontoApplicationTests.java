2405cdd7197aead8e7dfb477ea01a67d64f4d9b4
Ricardo Barreto de Araujo <rbbarreto@hotmail.com>
2021-07-14 23:52:03 -0300

    Add files via upload

---
+++ ponto/src/test/java/com/Live/ponto/PontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Live.ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
